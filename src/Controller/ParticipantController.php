af4ac68a3189ae9acc7442e81f2f60bcef76af79
wooody30 <jonathan.dovale@hotmail.fr>
2023-08-22 10:12:11 +0200

    crea lien profile user

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -10,9 +10,19 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/', name: 'app_participant')]
 class ParticipantController extends AbstractController
 {
-    #[Route('/affichageProfil/{participant}', name: '_affichageProfil')]
-    public function affichageProfil(Participant $participant): Response
+    #[Route('/affichageParticipant/{participant}', name: '_affichageParticipant')]
+    public function affichageParticipant(Participant $participant): Response
     {
-        return $this->render('participant/profil.html.twig', compact('participant'));
+        return $this->render('participant/participant.html.twig', compact('participant'));
+    }
+    #[Route('/affichageProfil', name: '_affichageProfil')]
+    public function affichageUser(): Response
+    {
+        $participant = $this->getUser(); // Récupère l'utilisateur connecté
+
+        if (!$participant instanceof Participant) {
+            throw $this->createAccessDeniedException('Vous devez être connecté en tant que participant.');
+        }
+        return $this->render('participant/profilUser.html.twig', compact('participant'));
     }
 }
