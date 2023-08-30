304e80bf0ac9d6bc8b4f433e2131cb5f4c0eaa8f
JuOkami <justin.candela@gmail.com>
2023-08-30 14:19:02 +0200

    suite du css

---
+++ public/JS/formulairederecherche.js
@@ -0,0 +1,23 @@
+let barre = document.getElementById("zonederecherche");
+let etat = false;
+console.log('bienvenue dans le script');
+console.log(barre);
+
+window.onload = init;
+
+function init(){
+    console.log("init")
+    barre.addEventListener('click', affichage);
+}
+
+function affichage() {
+    console.log("bienvenue dans la méthode")
+    if (!etat){
+        document.getElementById("formulairederecherche").style.display = "block";
+        document.getElementById("formulairederecherche").style.scale = "(1, 1)";
+        etat = true;
+    } else {
+        document.getElementById("formulairederecherche").style.display = "none";
+        etat = false;
+    }
+}
